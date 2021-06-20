49ac4746ae6f7ed81a3ba81d2b404b8c163b4b09
Walrussuit101 <tjefferson@ycp.edu>
2021-06-20 16:21:04 -0400

    add [as] text using animate.css
    and position w/ bootstrap grid system

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import "./App.css";
+
 function App() {
-  return (
-    <h4>hello</h4>
-  );
+	return (
+		<Container id='as-text-container'>
+			<Row className='justify-content-center'>
+				<Col className='text-center animate__animated animate__fadeInLeft px-0' xs='auto'>
+					<h1 className='as-text'>[a</h1>
+				</Col>
+				<Col className='text-center animate__animated animate__fadeInRight px-0' xs='auto'>
+					<h1 className='as-text'>s]</h1>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
